be22142d35529596b00ea31721fe21d524200cb7
boszarcode <87903203+boszarcode@users.noreply.github.com>
2025-02-09 19:31:07 +0000

    Fibonacci to user input digits
    - Fibonacci program that runs the amount of times the user sets.
    - Inside a function

--- playground/small_projects/fibonacci.py
+++ playground/small_projects/fibonacci.py
@@ -1,1 +1,26 @@
 # fibonacci calculator to x amount of places
+
+import sys
+
+sys.set_int_max_str_digits(50000)
+
+second = int(input("Fibonacci how many times? >> "))
+
+
+def fib_seq(second):
+    a = 0
+    b = 1
+    c = 0
+
+    print(b)
+
+    for x in range(1, second):
+        print(a + b)
+        print('')
+        c = a + b
+        a = b
+        b = c
+    return
+
+
+fib_seq(second)
